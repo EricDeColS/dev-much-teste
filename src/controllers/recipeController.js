1550ef68af2bbbcdb398e1aa10143140fc35755e
Eric Sales <ericdecols@gmail.com>
2020-07-08 09:12:56 -0300

    controller criado

---
+++ src/controllers/recipeController.js
@@ -0,0 +1,9 @@
+import getGif from '../api/giphyAPI';
+import getRecipe from '../api/recipePuppyAPI';
+
+export default async function recipeController(req, res) {
+
+    console.log(req.query);
+
+    return
+}
