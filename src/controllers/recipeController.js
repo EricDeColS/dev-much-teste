7032552d481b90d23485213cb8a8949dcf155944
Eric Sales <ericdecols@gmail.com>
2020-07-08 18:29:49 -0300

    Gif é inserido na receita no controller Receita não é mais criada em recipePuppyService

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -3,18 +3,35 @@ import getRecipe from '../api/recipePuppyService';
 
 export default async function recipeController(req, res) {
 
-    const ing = req.query.i;
-    if (ing.length === 0) {
+    const ingredients = req.query.i;
+    if (ingredients.length === 0) {
         return res.status(400).send({
-            message: 'AAAAAAAAAAAAAAA',
+            message: 'There`s nothing to be sniffed by puppies here, try adding ingredients or check for valid characters',
         });
     }
-    const ingredientsArray = ing.split(',');
-    console.log("array de ing: " + ingredientsArray);
-    const recievedRecipe = await getRecipe(ing);
+    const ingredientsArray = ingredients.split(',');
+    // falta conferir quantidade de ingredientes.
     
-    const gif = await getGif(recievedRecipe.title);
-    console.log("titulo da receita: " + recievedRecipe.title);
+    const recipesListed = {
+        "keywords" : [...ingredientsArray],
+        "recipes": []
+    }
 
-    return res.status(200).send({keywords: ingredientsArray, recipes: recievedRecipe });
+    const recievedRecipeArray = await getRecipe(ingredients);
+    const recipes = recievedRecipeArray.data.results.map(async r =>{
+        const{title, ingredients, href} = r;
+        
+        const giphy = await getGif(r.title);
+        const image = giphy.data.data[0].images.downsized_large.url;
+        
+        recipesListed.recipes.push({
+            title: title,
+            ingredients: ingredients,
+            link: href,
+            gif: image
+        });
+    });
+    Promise.all(recipes).then(()=>{
+        return res.status(200).send(recipesListed);
+    })
 }
