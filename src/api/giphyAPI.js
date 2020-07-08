fb469f00615d498319f3af84f0a426f4794f0e91
Eric Sales <ericdecols@gmail.com>
2020-07-08 09:12:01 -0300

    apis giphy e recipe puppy adicionadas

---
+++ src/api/giphyAPI.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const api = axios.create({
+    baseURL: 'https://api.giphy.com/v1/gifs/'
+});
+
+export default async function getGif(recipe) {
+    const q = `search?api_key=${process.env.GIPHY_API_KEY}&q=${recipe}&limit=1`;
+
+    const res = await api.get(q);
+    if (res.status !== 200) {
+        return {
+            error: 'Oh no! Our gifator is not gifing at the moment, try again later or call out support.'
+        };
+    }
+
+    if (!res.data.data)
+        return {
+            error: 'Wow, your search doesn`t have a gif yet, did you just invented this?'
+        };
+    
+    return res.data.data[0].url;
+}
---
+++ src/api/recipePuppyAPI.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL:'http://www.recipepuppy.com/api/'
+});
+
+export default async function recipes(i) {
+    const res = await api.get(`?i=${i}`);
+
+    if(res.status !== 200) {
+        return {
+            error: 'It looks like the puppies are asleep, let`s wake them, try again later'
+        };
+    }
+
+    if(res.data.results.length < 1) {
+        return {
+            error: 'The puppies sniffed for goodies but nothing was found! It looks like you have something never sniffed before '
+        };
+    }
+
+    const recipes  = res.data.results.map((recipe) => {
+        const i = recipe.ingredients.split(',');
+        return {
+            title: recipe.title,
+            href: recipe.href,
+            ingredients: i.sort()
+        };
+    });
+    return recipes;
+}
