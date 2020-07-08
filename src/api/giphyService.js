35caac1ab9c8c8e9707cd4d1bb9a82f85273aebf
Eric Sales <ericdecols@gmail.com>
2020-07-08 18:23:15 -0300

    retorno modificado para o Response completo

--- src/api/giphyService.js
+++ src/api/giphyService.js
@@ -7,8 +7,8 @@ const api = axios.create({
     baseURL: 'https://api.giphy.com/v1/gifs/'
 });
 
-export default async function getGif(recipe) {
-    const query = `search?api_key=${process.env.GIPHY_API_KEY}&q=${recipe}&limit=1`;
+export default async function getGif(recipeTitle) {
+    const query = `search?api_key=${process.env.GIPHY_API_KEY}&q=${recipeTitle}&limit=1`;
 
     const res = await api.get(query);
     if (res.status !== 200) {
@@ -18,9 +18,7 @@ export default async function getGif(recipe) {
     }
 
     if (!res.data.data)
-        return {
-            error: 'Wow, your search doesn`t have a gif yet, did you just invented this dish?'
-        };
-    
-    return res.data.data[0].url;
+        return '';
+        
+return res;
 }
