11b741376c1edd700874b9f635c267b158e0a4c9
Eric Sales <ericdecols@gmail.com>
2020-07-08 15:26:04 -0300

    correcoes no routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,6 @@ import recipeController from '../controllers/recipeController'
 
 const routes = new Router();
 
-routes.get('/recipes', recipeController());
+routes.get('/recipes', recipeController);
 
 export default routes;
