e15dd92b13db24a900d96e5e881aa0cf95356c2d
Eric Sales <ericdecols@gmail.com>
2020-07-08 23:06:38 -0300

    log removed

--- src/api/recipePuppyService.js
+++ src/api/recipePuppyService.js
@@ -7,9 +7,6 @@ const api = axios.create({
 export default async function recipes(i) {
     const res = await api.get(`?i=${i}`);
     
-    console.log("status: " + res.status);
-
-
     if(res.status !== 200) {
         return {
             error: 'It looks like the puppies are asleep, let`s wake them, try again later'
